3362c2ec7370cc64d59691b8ae5edb9fe1ba22b9
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2024-07-16 17:49:19 +0900

    디자인 초기화 세팅 및 mixin을 활용한 flexbox,position 속성 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './styles/main.scss';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Suspense } from 'react';
+
+const Login = lazy(() => import('./components/Login/Login'));
+const Mainpage = lazy(() => import('./components/main/MainPage'));
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+    <Suspense fallback="loading...">
+    <Routes>
+      <Route path='/' element={<Login/>}/>
+      <Route path='/home' element={<Mainpage/>}/>
+    </Routes>
+    </Suspense>
+   
+    </BrowserRouter>
   )
 }
 
