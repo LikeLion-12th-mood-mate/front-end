e6ab7a8c63124df3e43b33b775c6038ca1d00ce4
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2024-07-18 01:15:14 +0900

    implement button,input components

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 function Login() {
   return (
-    <div>Login</div>
+    <div className='container'>
+      <div className='row'>
+        <div className='col-sm-5' style={{backgroundColor:'hotpink'}}>
+          <input className='input-email active' placeholder='이메일'/>
+        </div>
+        <div className='col-sm-2' style={{backgroundColor:'hotpink'}}>test</div>
+      </div>
+    </div>
   )
 }
 
