e3529847e384b05f840d071c325df901aa77c210
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2024-07-20 04:24:34 +0900

    인증메일 카운터기능과 모달 애니메이션 추가

--- src/components/signup/Complete.jsx
+++ src/components/signup/Complete.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 import highfive from '../../assets/signup/highfive.svg'
-import { Link } from 'react-router-dom'
 function Complete() {
   return (
-    <div className='complete'>
+    <section className='complete'>
         <div className='container'>
             <div className='row'>
                 <div className='col-sm-5'>
@@ -12,13 +12,13 @@ function Complete() {
                         <h1 className='complete-title'>회원가입을 완료했어요<br/>MoodMate에 오신걸 환영해요</h1>
                         <span className='complete-image' style={{backgroundImage:`url(${highfive})`}}/>
                         <Link to='/'>
-                            <button className='complete-button' >다음</button>
+                            <button className='complete-button' >확인</button>
                         </Link>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
+    </section>
     
   )
 }
