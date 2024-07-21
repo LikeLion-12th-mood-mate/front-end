f8281bdef7aee9af8bc9029c7606d608bcd8cc2f
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2024-07-21 12:49:22 +0900

    라우터 children 속성을 이용

--- src/components/category/Category.jsx
+++ src/components/category/Category.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 function Category() {
   return (
-    <div>Category</div>
+    <div>
+      Category
+      <Outlet/>
+    </div>
   )
 }
 
--- src/components/diary/Diary.jsx
+++ src/components/diary/Diary.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 function Diary() {
   return (
-    <div>Diary</div>
+    <div>
+      Diary
+      <Outlet />
+    </div>
   )
 }
 
