035fe55e493c3d463e4935e645556fa075b80748
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2024-07-27 23:42:06 +0900

    implement search component

--- src/components/chat/KeywordButton.jsx
+++ src/components/chat/KeywordButton.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 
 
-function KeywordButton() {
+function KeywordButton(props) {
   return (
     <section className='keyword-wrap'>
-        <button className='keyword'>
+        <button className={props.kindOfKeyword==='빠른상담'?'keyword selected' :'keyword'} onClick={()=>props.handleKeyword('빠른상담')}>
             <span>😊</span>빠른상담
         </button>
-        <button className='keyword'><span>👍</span> 별점 높은 순</button>
-        <button className='keyword'><span>✍️</span> 상담 의뢰 순</button>
-        <button className='keyword'><span>😊</span> 최근 등록된 상담사</button>
+        <button className={props.kindOfKeyword==='별점높은순'?'keyword selected' :'keyword'} onClick={()=>props.handleKeyword('별점높은순')}><span>👍</span> 별점 높은 순</button>
+        <button className={props.kindOfKeyword==='상담의뢰순'?'keyword selected' :'keyword'} onClick={()=>props.handleKeyword('상담의뢰순')}><span>✍️</span> 상담 의뢰 순</button>
+        <button className={props.kindOfKeyword==='최근등록된상담사'?'keyword selected' :'keyword'} onClick={()=>props.handleKeyword('최근등록된상담사')}><span>😊</span> 최근 등록된 상담사</button>
     </section>
   )
 }
