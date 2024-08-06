da8d2e94922ef597a5edfa42ab47ffb1a399a3c0
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2024-08-06 22:12:26 +0900

    [update] 일기 조언 NaN 처리 완료

--- src/components/diary/analyst/Advice.jsx
+++ src/components/diary/analyst/Advice.jsx
@@ -7,13 +7,29 @@ function Advice(props) {
         <h3>오늘의 조언</h3>
         <div className='advice-wrap'>
             <h4>오늘의 조언 총평!</h4>
-            {data?.map((item)=>(
-              <>
+            {data?.map((item)=>
+            {
+              const [value3, value4] = item.analyze.split(' ').map(String);
+              return(
+                <>
+                {value3==='NaN' && value4==='NaN' ?
+                <div>
+                <p className='none'>분셕결과 없습니다.</p>
+          
+            </div>
+                :
+                <>
                 <p>{item.analysisMessage}</p>
                 <p>{item.encouragementMessage}</p>
+                </>  
+                
+              }
+                  
               </>
+              )
+             
               
-            )) }
+}) }
         </div>
     </>
   )
